ee9189e06127a567f31d11516e9c7b7701217b06
BraydenChumbley <bchumbley2001@gmail.com>
2023-03-31 12:58:38 -0400

    Added Course class

--- CourseConnect/src/course/Course.java
+++ CourseConnect/src/course/Course.java
@@ -1,5 +1,69 @@
 package course;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Course {
+
+    private Subject subject;
+    private Integer courseCode;
+    private Float weight;
+
+    private ArrayList<Course> preReqs;
+
+    public Course() {
+        this.subject = Subject.NULL;
+        this.courseCode = 0;
+        this.weight = 0.0f;
+        this.preReqs = new ArrayList<Course>(0);
+    }
+
+    public Course(Subject _subject, Integer _courseCode, Float _weight) {
+        this();
+        this.subject = _subject;
+        this.courseCode = _courseCode;
+        this.weight = _weight;
+    }
+
+    public Course(Subject _subject, Integer _courseCode, Float _weight, Course[] _preReqs) {
+        this(_subject, _courseCode, _weight);
+        this.preReqs.addAll(Arrays.asList(_preReqs));
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Integer getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(Integer courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public ArrayList<Course> getPreReqs() {
+        return preReqs;
+    }
+
+    public void setPreReqs(ArrayList<Course> preReqs) {
+        this.preReqs = preReqs;
+    }
+
+    public String toString() {
+        return subject.toString() + "*" + String.format("%04d", courseCode);
+    }
     
 }
