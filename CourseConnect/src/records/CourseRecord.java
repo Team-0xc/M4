c399f1fab4580e3c1c693efde8d33fb8ca48dd87
BraydenChumbley <bchumbley2001@gmail.com>
2023-03-31 21:34:57 -0400

    Added Transcript implementation

---
+++ CourseConnect/src/records/CourseRecord.java
@@ -0,0 +1,45 @@
+package records;
+
+import course.Course;
+
+public class CourseRecord {
+
+    public enum Status {
+        IN_PROGRESS, COMPLETED, FAILED
+    }
+
+    private Course course;
+    private Status status;
+    private Float grade;
+    
+    public CourseRecord(Course _course, Status _status, Float _grade) {
+        this.course = _course;
+        this.status = _status;
+        this.grade = _grade;
+    }
+
+    public Course GetCourse() {
+        return course;
+    }
+
+    public void SetCourse(Course course) {
+        this.course = course;
+    }
+
+    public Status GetStatus() {
+        return status;
+    }
+
+    public void SetStatus(Status status) {
+        this.status = status;
+    }
+
+    public Float GetGrade() {
+        return grade;
+    }
+
+    public void SetGrade(Float grade) {
+        this.grade = grade;
+    }
+
+}
--- CourseConnect/src/records/Transcript.java
+++ CourseConnect/src/records/Transcript.java
@@ -1,5 +1,65 @@
 package records;
 
+import java.util.Collection;
+import java.util.HashMap;
+
+import course.Course;
+
 public class Transcript {
     
+    private HashMap<String, CourseRecord> courseRecords;
+
+    public Transcript() {
+        this.courseRecords = new HashMap<String, CourseRecord>(0);
+    }
+
+    public Transcript(Collection<CourseRecord> _courseRecords) {
+        this();
+        // this.courseRecords.addAll(_courseRecords);
+        for(CourseRecord cr: _courseRecords) {
+            this.courseRecords.put(cr.GetCourse().GetHash(), cr);
+        }
+    }
+
+    public void PutCourse(CourseRecord cr) {
+        this.courseRecords.put(cr.GetCourse().GetHash(), cr);
+    }
+
+    public void RemoveCourse(Course c) {
+        this.courseRecords.remove(c.GetHash());
+    }
+
+    public boolean HasPassed(Course c) {
+        return this.courseRecords.get(c.GetHash()).GetStatus() == CourseRecord.Status.COMPLETED;
+    }
+
+    public Float GetTotalCredits() {
+
+        Float totalCredits = 0.0f;
+
+        // Add all weights
+        for(CourseRecord cr: this.courseRecords.values()) {
+            totalCredits += cr.GetCourse().GetWeight();
+        }
+
+        return totalCredits;
+
+    }
+
+    public Float GetGPA() {
+
+        // Check for potential divide by 0
+        if(this.courseRecords.size() == 0) {
+            return 0.0f;
+        }
+
+        Float totalGPA = 0.0f;
+
+        for(CourseRecord cr: this.courseRecords.values()) {
+            totalGPA += cr.GetGrade();
+        }
+
+        return totalGPA / this.courseRecords.size();
+    }
+
 }
