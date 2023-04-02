b98d30b80a8e14757308fc9b982f8fad15b922ce
ekozatch <ekozatch@uoguelph.ca>
2023-04-02 14:07:56 -0400

    implement Location class

--- CourseConnect/src/course/Location.java
+++ CourseConnect/src/course/Location.java
@@ -2,4 +2,48 @@ package course;
 
 public class Location {
     
+    public Location(){
+        this("", 0, False);
+    }    
+
+    public Location (String building, int roomNum, Boolean isOnline){
+        this.building = building;
+        this.roomNum = roomNum;
+        this.isOnline = isOnline
+    }
+
+    public GetBuilding(){
+        return building;
+    }
+
+    public SetBuilding(String building){
+        this.building = building;
+    }
+
+    public GetRoomNum(){
+        return roomNum;
+    }
+
+    public SetRoomNum(int roomNum){
+        this.roomNum = roomNum;
+    }
+
+    public MeetingDesc(){
+        String format = "";
+
+        if isOnline{
+            format = "Online";
+        }
+        else{
+            format = "In Person";
+        }
+
+        return "Building: " + building +
+             "Room Number: " + roomNum + 
+             "Format: " + format;
+
+    }
+
+
+
 }
