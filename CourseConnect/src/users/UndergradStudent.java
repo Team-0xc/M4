c69d0b9318eb93649efa4c40ed6928012cb7cf7c
BraydenChumbley <bchumbley2001@gmail.com>
2023-04-02 21:40:02 -0400

    Modified student subclasses to use 1 unified loop function

--- CourseConnect/src/users/UndergradStudent.java
+++ CourseConnect/src/users/UndergradStudent.java
@@ -1,9 +1,5 @@
 package users;
 
-import java.util.Scanner;
-
-import main.CourseConnect;
-
 public class UndergradStudent extends Student {
     public UndergradStudent(int _ID, String _firstName, String _lastName) {
         super(_ID, _firstName, _lastName);
