53875643f1838c74f435256ff06f6b6dcfef3059
BraydenChumbley <bchumbley2001@gmail.com>
2023-04-03 00:24:51 -0400

    Added switch user capability

--- CourseConnect/src/users/Professor.java
+++ CourseConnect/src/users/Professor.java
@@ -3,31 +3,35 @@ package users;
 import main.CourseConnect;
 
 public class Professor extends Person{
-        private String credentials;
+    private String credentials;
 
-        public Professor(String _firstName, String _lastName, String _credentials) {
-            super(_firstName, _lastName);
+    public Professor(String _firstName, String _lastName, String _credentials) {
+        super(_firstName, _lastName);
 
-            credentials = _credentials;
-        }
+        credentials = _credentials;
+    }
 
-        /**
-         * Sets the professors credentials
-         * 
-         * @param _credentials String: the new credentials
-         */
-        public void setCredentials(String _credentials) {
-            this.credentials = _credentials;
-        }
+    /**
+     * Sets the professors credentials
+     * 
+     * @param _credentials String: the new credentials
+     */
+    public void setCredentials(String _credentials) {
+        this.credentials = _credentials;
+    }
 
-        public void approveEnrollment() {
-            //TODO: the whole function for approving enrollement. How is this going to work, what does the fucntion take?
-        }
+    public void approveEnrollment() {
+        //TODO: the whole function for approving enrollement. How is this going to work, what does the fucntion take?
+    }
 
-        @Override
-        public void Loop(CourseConnect cc) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'Loop'");
-        }
+    @Override
+    public void Loop(CourseConnect cc) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Loop'");
+    }
 
+    private class EnrollmentRequest {
+        
+    }
 }
+
